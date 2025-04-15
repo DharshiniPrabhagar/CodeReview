7fa73ffd4581591e8c337622b5c09cc2ffb45ddb
DharshiniPrabhagar <dharshini.p@encora.com>
2025-04-16 02:22:43 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
-import os
-print("hi, how are you?")
+def fibonacci(n):
+    sequence = []
+    a, b = 0, 1
+    for _ in range(n):
+        sequence.append(a)
+        a, b = b, a + b
+    return sequence
+
+# Example usage
+print(fibonacci(10))
